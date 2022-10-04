559073ef39995f760bb9bc8b747fa7df880016ce
qlghwp123 <qlghwp123@naver.com>
2022-10-04 21:15:41 +0900

    Add solved problem

--- week_1/10808.cpp
+++ week_1/10808.cpp
@@ -1,3 +1,24 @@
 /*
 답 : http://boj.kr/1a1898996c8542889b32b4c1b2498dd0
 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    string S;
+    int ret[26] = {
+        0,
+    };
+
+    cin >> S;
+
+    for (auto i : S)
+        ret[i - 'a']++;
+
+    for (auto i : ret)
+        cout << i << ' ';
+
+    return 0;
+}
